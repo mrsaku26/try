849d046f0c4db78e3fcd60796cf600e2bf380b61
Demo <sangitkc37@gmail.com>
2025-10-09 14:16:54 +0545

    dd

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -13,28 +13,6 @@ try {
    console.error(error)
 }
 
-// Creating multer for images
-
-const storage = multer.diskStorage({
-  destination:'./Upload/images', 
-  filename (req, file, cb) {
-    cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-  },
-
-})
-
-const upload = multer({ storage: storage })
-
-app.use('/images',express.static('Upload/images'))
-
-// Creating Upload Endpoint for images
-
-app.post('/upload',upload.single('product'),(req,res)=>{
-  res.json({
-    success:1,
-    image_url:`http://localhost:${port}/images/${req.file.filename}`
-  })
-})
 
 
 app.get('/', (req, res) => {
