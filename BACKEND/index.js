592f73bc19b52e86533397cbb86953cde3c49179
Demo <sangitkc37@gmail.com>
2025-10-09 13:47:18 +0545

    ad

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,29 +1,11 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/db.js';
-import adminRouter from './Routes/adminRoutes.js';
-import blogRouter from './Routes/blogRoutes.js';
-
+const express = require('express')
 const app = express()
-const port =  5000;
+const port = 3000
 
-await connectDB()
-
-// Middlewares
-app.use(cors())
-app.use(express.json())
-
-// Routes
-app.get('/', (req, res) => {   res.send('Hello My Frinends!') })
-app.use('/api/admin',adminRouter)
-app.use('/api/blog',blogRouter)
-
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-
-export default app;
